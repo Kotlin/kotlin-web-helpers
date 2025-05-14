65d462b40e0dfa78a4afa7ec04c410da5299ae53
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-05-14 14:16:44 +0200

    Add webpackCopy to copy additional files

--- src/KarmaWebpackOutputFramework.mjs
+++ src/KarmaWebpackOutputFramework.mjs
@@ -3,6 +3,9 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
+import fs from "fs";
+import path from "path";
+
 export function KarmaWebpackOutputFramework(config) {
     // This controller is instantiated and set during the preprocessor phase.
     const controller = config.__karmaWebpackController;
@@ -16,11 +19,22 @@ export function KarmaWebpackOutputFramework(config) {
         return
     }
 
+    // make sure tmp folder exists
+    const outputPath = controller.outputPath;
+
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath);
+    }
+
     config.files.push({
-        pattern: `${controller.outputPath}/**/*`,
+        pattern: `${outputPath}/**/*`,
         included: false,
         served: true,
         watched: false
+    })
+
+    config.webpackCopy?.forEach(file => {
+        fs.copyFileSync(file, path.resolve(outputPath, path.basename(file)));
     })
 }
 
